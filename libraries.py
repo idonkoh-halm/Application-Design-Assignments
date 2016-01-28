3550bee6d5d9cbb8a08c17cc995fc31715b42e91
idonkoh-halm <isaac.donkoh-halm@innovationcharter.org>
2016-01-28 09:05:17 -0500

    uh oh

--- libraries.py
+++ libraries.py
@@ -7,7 +7,17 @@ def math_excercise():
     sqrt_table=[]
     for i in range (1,101):
         squarval=math.sqrt(i)
-        sqrt_table.append(squarval)
+        sqrt_table.append([squarval])
     print sqrt_table    
 
 math_excercise()
+
+def csv_excercise():
+    with open('sqrttable.csv','a') as csvfile:
+        squarereader=csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in squarereader:
+              print ', '.join(row)
+
+#with file ('sqrtables.csv','w'):
+    
+csv_excercise()
